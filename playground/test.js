96b4f2e9a8571a0e65765894cdd280d683f508b7
John Lin <johnlin72499@gmail.com>
2020-03-11 10:25:47 -0400

    update scraped profiles and follow script

--- playground/test.js
+++ playground/test.js
@@ -1,14 +1,16 @@
-let buttons = null;
+function doFollows() {
+    let buttons = null;
 
-buttons = Array.from(document.querySelectorAll('._0mzm-.sqdOP.L3NKy:not(._8A5w5)'));
+    buttons = Array.from(document.querySelectorAll('.sqdOP.L3NKy.y3zKF:not(._8A5w5)'));
 
-buttons.reduce((chain, button) => {
-    return chain.then(() => {
-        button.click();
-        return new Promise((resolve) => {
-            setTimeout(resolve, 1000);
+    buttons.reduce((chain, button) => {
+        return chain.then(() => {
+            button.click();
+            return new Promise((resolve) => {
+                setTimeout(resolve, 600);
+            });
         });
-    });
-}, Promise.resolve());
+    }, Promise.resolve());
 
-console.log(buttons.length);
+    console.log(buttons.length);
+}
